827f44cb2d48629dbc04d1828faf9bdfcdbfe0fc
Borah557 <bniva29@gmail.com>
2025-04-03 13:14:47 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from telegram.ext import Application, CommandHandler
 async def start(update, context):
     await update.message.reply_text('Hello from Railway!')
 
-app = Application.builder().token("7972508793:AAGliWtKAbzXCEM4cvZYgB4JRgzG4HSpxeY").build()
+app = Application.builder().token("bot-token-here").build()
 app.add_handler(CommandHandler("start", start))
 app.run_polling()
